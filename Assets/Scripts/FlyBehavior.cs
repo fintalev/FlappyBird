a9b40471663fc81792076972ce82be2cf30b89b6
fintalev <112973569+fintalev@users.noreply.github.com>
2023-11-23 13:08:48 +0100

    BirdDieXD

--- Assets/Scripts/FlyBehavior.cs
+++ Assets/Scripts/FlyBehavior.cs
@@ -28,4 +28,9 @@ public class FlyBehavior : MonoBehaviour
     {
         transform.rotation=Quaternion.Euler(0,0,rigidbody2D.velocity.y*_rotationSpeed);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameManager.instance.GameOver();
+    }
 }
